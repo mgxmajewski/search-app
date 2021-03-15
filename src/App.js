0f03fef611c128cb7c0d13f8b859165386d8f83a
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-15 21:53:00 +0100

    Add bootstrap and render Jumbotron with container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Container, Jumbotron } from "react-bootstrap";
+
 
 function App() {
   return (
     <div >
-
+      <Jumbotron>
+        <Container>
+          <h1>Search App</h1>
+          <p>This is a simple search app</p>
+        </Container>
+      </Jumbotron>
     </div>
   );
 }
